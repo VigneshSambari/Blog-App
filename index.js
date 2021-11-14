48dcde9cd4c25579c72f90c64ee2af2af310b07e
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2021-11-14 20:19:30 +0530

    port fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
-const Port=process.env.port||2000;
+const Port=process.env.PORT||2000;
 mongoose.connect("mongodb+srv://bloguser:vicky1234@cluster0.7ngse.mongodb.net/blogappdatabase?retryWrites=true&w=majority",
 {
     useNewUrlParser:true,
